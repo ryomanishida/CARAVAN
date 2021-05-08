f742a7409650e1c3e5520394156f61066ac1f3cd
ryomanishida <riyoma5407@gmail.com>
2021-05-08 08:42:13 +0000

    [Finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,27 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog=Blog.new
+  end
+
+  def create
+    blog=Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title,:category,:body)
+  end
+
 end
